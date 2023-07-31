82660b6c4a27921bfffebdde133af94910296604
Daniel Sklyarov <sklyarov2023@outlook.com>
2023-07-31 18:44:08 +0300

    build a NavBar

---
+++ src/NavBar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../src/assets/logo.webp";
+const NavBar = () => {
+  return (
+    <div>
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+        <Text fontSize="xl" as="b">
+          GameHub
+        </Text>
+      </HStack>
+    </div>
+  );
+};
+
+export default NavBar;
