894a63f8ee50f0d539f8fb257861356618689c41
Daniel Sklyarov <sklyarov2023@outlook.com>
2023-08-02 16:42:23 +0300

    add loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,15 +1,24 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />{" "}
+    <Card borderRadius={10} overflow={"hidden"} width="300px">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,20 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import { FaSadCry } from "react-icons/fa";
 
+export interface Platform{
+  id:	number;
+  slug:	string;
+  name:	string;
+}
 
 export interface Game {
     id: number;
     name: string;
     background_image:string
+    parent_platforms: {platform:Platform}[]
+    metacritic: number
   }
   
   interface FetchGameResponse {
@@ -16,20 +24,23 @@ export interface Game {
 
 const useGames = ()=>{
     const controller = new AbortController()
+
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const  [isLoading,setLoading] = useState(false)
   
     useEffect(() => {
+      setLoading(true)
       apiClient
         .get<FetchGameResponse>("/games",{signal:controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {setGames(res.data.results); setLoading(false)})
         .catch((err) => {if (err instanceof CanceledError) return; 
           setError(err.message)});
 
         return () => controller.abort()
     },[]);
 
-    return {games,error} 
+    return {games,error, isLoading} 
 }
 
 export default useGames
